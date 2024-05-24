59a5aca756abb2580fec4b4cab784e99fc1c8b8e
Vishal <dvishal485@gmail.com>
2024-05-24 17:24:23 +0530

    parse steps from arbitary config
    Signed-off-by: Vishal <dvishal485@gmail.com>

--- src/browser/shepherd_injection.ts
+++ src/browser/shepherd_injection.ts
@@ -1,50 +1,45 @@
-import type S from "shepherd.js";
+import build from "../tour_builder";
+
+const dummy_config = `[
+  {
+    "id": "email",
+    "text": "email elem",
+    "attachTo": {
+      "element": "#email",
+      "on": "bottom"
+    },
+    "classes": "email",
+    "tasks": [
+      "next"
+    ],
+    "buttons": [
+      {
+        "text": "Next"
+      }
+    ]
+  },
+  {
+    "id": "password",
+    "text": "pwd elem",
+    "attachTo": {
+      "element": "#password",
+      "on": "bottom"
+    },
+    "tasks": [
+      "next"
+    ],
+    "classes": "password",
+    "buttons": [
+      {
+        "text": "Next"
+      }
+    ]
+  }
+]`;
 
 (function () {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const Shepherd: typeof S = require("shepherd.js");
-
   console.log("Shepherd injection!");
 
-  const injectedTour = new Shepherd.Tour({
-    useModalOverlay: false,
-    defaultStepOptions: {
-      classes: "shadow-md bg-purple-dark",
-      scrollTo: true,
-    },
-  });
-
-  injectedTour.addStep({
-    id: "email",
-    text: "email elem",
-    attachTo: {
-      element: "#email",
-      on: "bottom",
-    },
-    classes: "email",
-    buttons: [
-      {
-        text: "Next",
-        action: injectedTour.next,
-      },
-    ],
-  });
-
-  injectedTour.addStep({
-    id: "password",
-    text: "pwd elem",
-    attachTo: {
-      element: "#password",
-      on: "bottom",
-    },
-    classes: "password",
-    buttons: [
-      {
-        text: "Next",
-        action: injectedTour.next,
-      },
-    ],
-  });
-
-  injectedTour.start();
+  const tour = build(dummy_config);
+  tour?.start();
 })();
---
+++ src/tour_builder.ts
@@ -0,0 +1,47 @@
+import S from "shepherd.js";
+
+export default function build_tour(config: string) {
+  try {
+    const config_json: TourSteps[] = JSON.parse(config);
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const Shepherd: typeof S = require("shepherd.js");
+
+    const injectedTour = new Shepherd.Tour({
+      useModalOverlay: false,
+      defaultStepOptions: {
+        classes: "shadow-md bg-purple-dark",
+        scrollTo: true,
+      },
+    });
+
+    type TourSteps = Parameters<typeof injectedTour.addStep>[0] & {
+      tasks: ["next" | "prev" | "cancel"];
+    };
+
+    config_json.forEach((step) => {
+      if ("buttons" in step) {
+        step.buttons?.forEach((button, idx) => {
+          button.action = () => {
+            switch (step.tasks[idx]) {
+              case "prev":
+                injectedTour.back();
+                break;
+              case "cancel":
+                injectedTour.cancel();
+                break;
+              case "next":
+              default:
+                injectedTour.next();
+            }
+          };
+        });
+      }
+      injectedTour.addStep(step);
+    });
+
+    return injectedTour;
+  } catch (e) {
+    console.error("Error building tour!", e);
+    return null;
+  }
+}
